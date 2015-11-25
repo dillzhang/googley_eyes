f44f07873c538f6340eaa81711aa985cad2a474a
bleepbloopsify <bleepbloopsify@gmail.com>
2015-11-25 01:11:11 -0500

    Fixed Yes No and Permalink Hide

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import google, urllib2, bs4, re
 
 secret_key = urandom(32)
 
-pattern = "[A-Z][a-z]+(?: [A-Z](?:[a-z]+|\.))? [A-Z][a-z]+"
+pattern = "[A-Z][a-z]+(?: [A-Z](?:[a-z]+|\.))? [A-Z][a-z\-]+[A-Z]*[a-z]*"
 
 def look_up(query_string):
     results = google.search(query_string, num=10, start=0, stop=10)
@@ -13,13 +13,14 @@ def look_up(query_string):
         try:
             url = urllib2.urlopen(result)
             page = url.read()
-            soup = bs4.BeautifulSoup(page, "html.parser")
+            soup = bs4.BeautifulSoup(page)
             raw = soup.get_text()
             clean = re.sub("[ \t\n]+", " ", raw)
             findings = re.findall(pattern, clean)
             result_list += findings
         except:
             print "error"
+    result_list = [r for r in result_list if r != "Yes No" and r != "Permalink Hide"]
     return most_common(result_list)
 
 def most_common(lst):
